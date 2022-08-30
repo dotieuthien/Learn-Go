270a33e6aa5bdb914a582ea2a85443dffd32e66f
dotieuthien <dotieuthien9997@gmail.com>
2022-08-31 00:12:37 +0700

    Test more goroutine

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -10,19 +11,37 @@ func main() {
 		"http://google.com",
 		"http://facebook.com",
 		"http://amazon.com",
-		"http://stackoverflow.com",
+		"htp://stackoverflow.com",
 	}
+	start_time := time.Now()
+	for _, link := range links {
+		checkLink(link)
+	}
+	fmt.Printf("Execution time %s\n", time.Since(start_time))
+
+	start_time = time.Now()
 	// create a string channel
 	c := make(chan string)
 
 	for _, link := range links {
 		// call go routine to execute checkLink func
+		// go routine need channel for communication with main routine
 		go checkLinks(link, c)
 	}
 	// listen the mesage from channel
 	for i := 0; i < len(links); i++ {
 		fmt.Println(<-c)
 	}
+	fmt.Printf("Execution time %s\n", time.Since(start_time))
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, " might be down!")
+		return
+	}
+	fmt.Println(link, " is up!")
 }
 
 func checkLinks(link string, c chan string) {
